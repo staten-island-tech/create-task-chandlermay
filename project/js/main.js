90db54cd5045d32f7d89e83981618a9eea624850
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-01-18 12:57:18 -0500

    1/18

--- project/js/main.js
+++ project/js/main.js
@@ -2,14 +2,13 @@ import '../style.css';
 import { DOMselectors } from './doms';
 import { characters } from './endor';
 
-for (let i=0 ; i < characters.length; i++) {
 DOMselectors.begin.addEventListener("click",function(){
     DOMselectors.planetcontainer.innerHTML = "";
+    const pic = random(characters.displayIndex);
     const card = `
     <h1 id="charactername">Who am I?</h1>
-    <img class="characterimg" alt="characterpicture" src="${character[i].displayIcon}">
+    <img class="characterimg" alt="characterpicture" src="${pic}">
     <input class="textbox" type="text">
     `
     DOMselectors.planetcontainer.insertAdjacentHMTL("afterbegin", card);
 })
-}
