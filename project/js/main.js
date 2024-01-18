1f6145876aac4467eed9818fb0126c4755587d17
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-01-18 13:13:45 -0500

    its jover

--- project/js/main.js
+++ project/js/main.js
@@ -1,13 +1,16 @@
 import '../style.css';
 import { DOMselectors } from './doms';
 import { characters } from './endor';
+import {shuffle} from './shuffle';
+import {number} from './shuffle';
 
 DOMselectors.begin.addEventListener("click",function(){
     DOMselectors.planetcontainer.innerHTML = "";
-    const pic = random(characters.displayIndex);
+    shuffle(characters.length);
+    alright = shuffle.result;
     const card = `
     <h1 id="charactername">Who am I?</h1>
-    <img class="characterimg" alt="characterpicture" src="${pic}">
+    <img class="characterimg" alt="characterpicture" src="${characters[alright].displayIcon}">
     <input class="textbox" type="text">
     `
     DOMselectors.planetcontainer.insertAdjacentHMTL("afterbegin", card);
