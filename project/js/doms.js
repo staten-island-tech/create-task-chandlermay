c88cd3b9c88453ea759b77f70a9009869c7d28cd
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-01-28 17:55:44 -0500

    1/28

--- project/js/doms.js
+++ project/js/doms.js
@@ -1,8 +1,8 @@
 const DOMselectors = {
     name: document.querySelector("#name"),
-    planetcontainer: document.querySelector(".planetcontainer"),
-    planetimg: document.querySelectorAll(".planetimg"),
-    begin:document.querySelector("#begin"),
+    container: document.querySelector(".container"),
+    img: document.querySelectorAll(".img"),
+    begin: document.querySelector(".begin"),
 }
 
 export {DOMselectors};
--- project/js/main.js
+++ project/js/main.js
@@ -1,26 +1,18 @@
 import '../style.css';
 import { DOMselectors } from './doms';
 import { characters } from './endor';
-import {shuffle} from './shuffle';
-import {number} from './shuffle';
 
-<<<<<<< Updated upstream
-=======
-// for (let i=0 ; i < characters.length; i++) {}
+function clearScreen(){
+    DOMselectors.container.innerHTML = ""
+}
 
->>>>>>> Stashed changes
 DOMselectors.begin.addEventListener("click",function(){
-    DOMselectors.planetcontainer.innerHTML = "";
-    shuffle(characters.length);
-    alright = shuffle.result;
+    clearScreen();
+    for (let i = 0; i < characters.length; i++){
     const card = `
-    <h1 id="charactername">Who am I?</h1>
-<<<<<<< Updated upstream
-    <img class="characterimg" alt="characterpicture" src="${characters[alright].displayIcon}">
-=======
-    <img class="characterimg" alt="characterpicture" src="${characters[i].displayIcon}">
->>>>>>> Stashed changes
-    <input class="textbox" type="text">
+    <h1 id="name">Who Am I?</h1>
+    <img class="img" alt="character-image" src="${characters[i].displayIcon}">
+    <input type="text">
     `
-    DOMselectors.planetcontainer.insertAdjacentHMTL("afterbegin", card);
-})
+    DOMselectors.container.insertAdjacentHTML("afterbegin", card)
+}})
