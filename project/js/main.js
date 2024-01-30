319859d6ee2f032133ea7ebf407f9fd0b98edbee
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-01-30 12:57:16 -0500

    removed shuffle and page load index 0

--- project/js/main.js
+++ project/js/main.js
@@ -2,17 +2,18 @@ import '../style.css';
 import { DOMselectors } from './doms';
 import { characters } from './endor';
 
+const i = 0
 function clearScreen(){
     DOMselectors.container.innerHTML = ""
 }
 
-DOMselectors.begin.addEventListener("click",function(){
-    clearScreen();
-    for (let i = 0; i < characters.length; i++){
-    const card = `
+function createCard(i){ 
+const card = `
     <h1 id="name">Who Am I?</h1>
     <img class="img" alt="character-image" src="${characters[i].displayIcon}">
     <input type="text">
     `
-    DOMselectors.container.insertAdjacentHTML("afterbegin", card)
-}})
+    DOMselectors.container.insertAdjacentHTML("afterbegin", card); 
+}
+
+createCard(0);
