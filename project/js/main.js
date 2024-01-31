7be2baf78158f6e528bf33343bc66c0203d660fe
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-01-31 13:15:18 -0500

    idk

--- project/js/main.js
+++ project/js/main.js
@@ -12,7 +12,6 @@ function clearLine() {
     DOMselectors.response.value = ""
 }
 
-
 function checkGuess() {
     const userGuess = DOMselectors.response.value;
     const correctGuess = characters[i].name.toLowerCase();
@@ -21,26 +20,24 @@ function checkGuess() {
 i++;
 clearScreen();
 createCard(i);
-clearLine();
     } else {
-clearScreen();
-DOMselectors.container.insertAdjacentHTML("afterbegin", "Not exactly...try again.");
-clearLine();
+DOMselectors.name.innerHTML = "Not quite, try again.";
     }
 }
 
 function createCard(i) {
     clearScreen();
     const card = `
-    <h1 id="name">Who Am I?</h1>
+    <p id="name">Who Am I?</p>
     <img class="img" alt="character-image" src="${characters[i].displayIcon}">
     <input type="text" id ="response">
     <button class="submit">Submit</button>
     `
     DOMselectors.container.insertAdjacentHTML("afterbegin", card);
-  DOMselectors.submit.addEventListener("click", checkGuess);  
+
 }
 
-
+//   DOMselectors.submit.addEventListener("click", checkGuess);  
 
 createCard(i);
+checkGuess();
